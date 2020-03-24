7effa91576929a6fb2ff0f6472309a861334a919
nevelson <nevelson92@gmail.com>
2020-03-24 14:55:03 -0400

    First commit

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+import shoppingList from './shopping-list';
+import $ from 'jquery';
+
+const main = function() {
+  shoppingList.bindEventListeners();
+  shoppingList.render();
+};
+
+$(main);
---
+++ scripts/item.js
@@ -0,0 +1,20 @@
+import cuid from 'cuid';
+
+function validateName(name) {
+  if (!name) {
+    throw new TypeError('Name must not be blank');
+  }
+}
+
+function create(name) {
+  return {
+    id: cuid(),
+    name,
+    checked: false
+  };
+}
+
+export default {
+  validateName,
+  create
+};
